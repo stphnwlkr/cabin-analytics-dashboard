d390e62740401d8e8ede16653fcdedaf0441b69b
Stephen Walker <iam@stphnwlkr.co>
2026-03-09 09:01:51 -0400

    Update PHP requirements and testing version

--- cabin-analytics-dashboard.php
+++ cabin-analytics-dashboard.php
@@ -5,7 +5,8 @@
  * Description:       Display Cabin Analytics data with interactive charts via dashboard widgets, blocks, and shortcodes.
  * Version:           1.0.1
  * Requires at least: 6.8.3
- * Requires PHP:      8.1
+ * Tested up to 7.0
+ * Requires PHP:      8.3
  * Author:            Stephen Walker
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
